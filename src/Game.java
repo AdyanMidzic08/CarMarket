936eda6738200e36a312024c60bd57ce02f32400
Finn <f.eser@students.htl-leonding.ac.at>
2026-06-16 14:27:11 +0200

    Added every methode from puml

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,15 @@
 public class Game {
+    private Player player;
 
+    public void init() {
+
+    }
+
+    public void startRound() {
+
+    }
+
+    public void play() {
+
+    }
 }
---
+++ src/Player.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class Player {
+    private int money;
+    private LinkedList<InventoryItem> inventory;
+
+    public int getItemCount(CarType type) {
+        return -1;
+    }
+
+    public void buy(CarType type, int amount) {
+
+    }
+
+    public void sell(CarType type, int amount) {
+
+    }
+}
